207a21e56694cdc5761e95b28b14fff4a5d7e3bc
ayushch80 <ayushch80@gmail.com>
2025-03-24 16:46:41 +0530

    fix: correct condition for checking supported ELF architecture

--- src/utils.c
+++ src/utils.c
@@ -98,7 +98,8 @@ bool isSupportedELF(char *filepath)
         exit(EXIT_FAILURE);
     }
 
-    if (ehdr.e_type != 0x02) {
+    if (ehdr.e_machine != 0x02)
+    {
         fprintf(stderr, "32-bit ELF isn't supported yet\n");
         close(fd);
         exit(EXIT_FAILURE);
